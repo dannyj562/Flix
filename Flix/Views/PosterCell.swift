3149e28ce67aae04364e5ac5c3f32418854126b7
dannyj562 <riveradanny51@gmail.com>
2018-09-14 18:54:15 -0700

    Setup and implement CollectionView on movies

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Danny on 9/14/18.
+//  Copyright © 2018 Danny Rivera. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+}
